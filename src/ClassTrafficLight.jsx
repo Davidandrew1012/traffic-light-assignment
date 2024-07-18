c39428d6f17c446ef8835222d90a4a63d6fcff9f
david01relic <davidandrew1012@gmail.com>
2024-07-18 09:36:50 -0400

    finished assignment

--- src/ClassTrafficLight.jsx
+++ src/ClassTrafficLight.jsx
@@ -1,18 +1,43 @@
 import { Component } from "react";
 
 export class ClassTrafficLight extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentState: 'red'
+    };
+  }
+
+  handleNextState = () => {
+    const { currentState } = this.state;
+    let nextState;
+    switch (currentState) {
+      case 'red':
+        nextState = 'yellow';
+        break;
+      case 'yellow':
+        nextState = 'green';
+        break;
+      case 'green':
+        nextState = 'red'
+    }
+    this.setState({ currentState: nextState });
+  };
+
+
   render() {
+    const { currentState } = this.state;
+
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${currentState === 'red' ? 'red' : 'black'}`}></div>
+          <div className={`circle ${currentState === 'yellow' ? 'yellow' : 'black'}`}></div>
+          <div className={`circle ${currentState === 'green' ? 'green' : 'black'}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.handleNextState}>Next State</button>
       </div>
-    );
+    )
   }
 }
--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,17 +1,33 @@
+import { useState } from 'react';
+
 export const FunctionalTrafficLight = () => {
+  const [currentState, setCurrentState] = useState('red');
+
+  const handleNextState = () => {
+    switch (currentState) {
+      case 'red':
+        setCurrentState('yellow');
+        break;
+      case 'yellow':
+        setCurrentState('green');
+        break;
+      case 'green':
+        setCurrentState('red');
+        break;
+      default:
+        setCurrentState('red')
+    }
+  };
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${currentState === 'red' ? 'red' : 'black'}`}></div>
+        <div className={`circle ${currentState === 'yellow' ? 'yellow' : 'black'}`}></div>
+        <div className={`circle ${currentState === 'green' ? 'green' : 'black'}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={handleNextState}>Next State</button>
     </div>
   );
-};
-
-
-
+}
